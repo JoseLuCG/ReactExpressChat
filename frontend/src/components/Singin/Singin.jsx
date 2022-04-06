61e97c569f0ff661c59f1e892281aba9973513a6
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-04-06 05:27:14 +0200

    Adds the inputs of the singin

--- frontend/src/components/Singin/Singin.jsx
+++ frontend/src/components/Singin/Singin.jsx
@@ -4,7 +4,10 @@ function Singin(){
     return(
         <div className="singinContainer">
         <h1>Esto es el singin</h1>
-
+        <p>Logueate.</p>
+        <input type="text" placeholder='Usuario'/>
+        <input type="password" placeholder='Constraseña' />
+        <button>Acceder</button>
         </div>
     )
 }
